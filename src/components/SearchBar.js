f8567d6997f5dcdaf5aec75757d1c44e4c001d1a
ThermoDev <michael.pegios@hotmail.com>
2020-05-20 04:46:07 +1000

    Initial Setup and Creating Search Bar

---
+++ src/components/SearchBar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+    state = { term: '' };
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+
+        this.props.onSubmit(this.state.term);
+    };
+
+    render() {
+        return (
+            <div className="ui segment">
+                <form onSubmit={this.onFormSubmit} className="ui form">
+                    <div className="ui field">
+                        <label>Search for a video</label>
+                        <input
+                            type="text"
+                            value={this.state.term}
+                            onChange={(e) =>
+                                this.setState({ term: e.target.value })
+                            }
+                            placeholder="Search..."
+                        />
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default SearchBar;
