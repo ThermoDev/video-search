26b959c220e7d8fecc90f7fc36681c7f4c2cb134
ThermoDev <michael.pegios@hotmail.com>
2020-05-23 00:55:24 +1000

    YouTube API Requests

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import SearchBar from './components/SearchBar';
+import axios from 'axios';
+
+const YOUTUBE_API_KEY = `${process.env.REACT_APP_YOUTUBE_API_KEY.toString()}`; /* Replace with your own API key from the YouTube API. */
 
 class App extends React.Component {
     onSearchSubmit = async (term) => {
-        // TODO: use term to make an API request
-       console.log(term);
+        const response = await axios.get(
+            'https://www.googleapis.com/youtube/v3/search',
+            {
+                params: {
+                    key: YOUTUBE_API_KEY,
+                    q: term,
+                },
+            }
+        );
+
+        console.log(response);
     };
 
-    
     render() {
         return (
-            <div className="ui container">
-                <SearchBar  onSubmit={this.onSearchSubmit} />
+            <div className="ui container" style={{ paddingTop: '20px' }}>
+                <SearchBar onSubmit={this.onSearchSubmit} />
             </div>
         );
     }
