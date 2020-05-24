b308958d33d39594f5f1ba12fd466e172315beba
ThermoDev <michael.pegios@hotmail.com>
2020-05-24 16:28:15 +1000

    Change VideoList to Class Based Component

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -2,14 +2,14 @@ import './VideoList.css';
 import React from 'react';
 import VideoItem from './VideoItem';
 
-
-const VideoPlayer = (props) => {
-    // console.log(props.videos);
-    const Titles = props.videos.map((video) => {
-        return <VideoItem key={video.id.videoId} video={video} />;
-    });
-    // console.log(Titles);
-    return <ul className="column">{Titles.slice(1,5)}</ul>;
-};
+class VideoPlayer extends React.Component {
+    render() {
+        const Titles = this.props.videos.map((video) => {
+            return <VideoItem key={video.id.videoId} video={video} />;
+        });
+        // console.log(Titles);
+        return <ul className="column">{Titles.slice(1, 5)}</ul>;
+    }
+}
 
 export default VideoPlayer;
