afc311d310b8e437c9a5ad0649f179831895651c
ThermoDev <michael.pegios@hotmail.com>
2020-05-30 00:21:50 +1000

    Updates

---
+++ precache-manifest.1a2d54228c844cf7c5a586d9e9b1da7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92bb09b6202f7dbf522073e11ad96d10",
+    "url": "/video-search/index.html"
+  },
+  {
+    "revision": "db92f85f92ab97e9d75a",
+    "url": "/video-search/static/css/main.6e960a21.chunk.css"
+  },
+  {
+    "revision": "ca370f339c9f111adc62",
+    "url": "/video-search/static/js/2.952aaf96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/video-search/static/js/2.952aaf96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db92f85f92ab97e9d75a",
+    "url": "/video-search/static/js/main.3725eb33.chunk.js"
+  },
+  {
+    "revision": "19482773e4ea79a7388c",
+    "url": "/video-search/static/js/runtime-main.5a4e9f6e.js"
+  }
+]);
